a8f46e9758fb8916c715f6bb001cd76475e7bdcd
AFCMS <afcm.contact@gmail.com>
2021-08-17 16:35:00 +0200

    .

--- mods/cs_menu/init.lua
+++ mods/cs_menu/init.lua
@@ -3,14 +3,14 @@ local table = table
 cs_menu = {}
 
 cs_menu.formspec_prepend = table.concat({
-	"bgcolor[#080808BB;true]",
-	"listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF",
+	"bgcolor[#100E1DBB;true]",
+	"listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]",
 	"background9[5,5;1,1;gui_formbg.png;true;10]",
 })
 
 minetest.register_on_joinplayer(function(player)
 	--disable inventory formspec
-	player:set_inventory_formspec("")
+	player:set_inventory_formspec("no_prepend[]")
 	--apply formspec styling
 	player:set_formspec_prepend(cs_menu.formspec_prepend)
 	--hide unused HUD elements
