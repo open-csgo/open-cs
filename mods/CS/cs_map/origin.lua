40d3d9bff33a23028513a281ced4627776d369e5
AFCMS <afcm.contact@gmail.com>
2022-01-13 20:39:36 +0100

    fix generation with dummy backend

--- mods/CS/cs_map/origin.lua
+++ mods/CS/cs_map/origin.lua
@@ -1,7 +1,5 @@
 local vector = vector
 
-local s = minetest.get_mod_storage()
-
 cs_map.origin_pos = vector.new(0, -1, 0)
 
 minetest.register_node("cs_map:origin",{
@@ -13,13 +11,10 @@ minetest.register_node("cs_map:origin",{
 	drop = "",
 })
 
-if s:get_int("origin_generated") == 0 then
-	minetest.register_on_generated(function(minp, maxp, seed)
-		local blockpos = cs_map.origin_pos
-		if(minp.x <= blockpos.x and maxp.x >= blockpos.x and minp.y <= blockpos.y and maxp.y >= blockpos.y and minp.z <= blockpos.z and maxp.z >= blockpos.z) then
-			minetest.set_node(blockpos, {name = "cs_map:origin"})
-			minetest.log("action", "[cs_map] origin has been set at "..minetest.pos_to_string(blockpos)..".")
-			s:set_int("origin_generated", 1)
-		end
-	end)
-end
+minetest.register_on_generated(function(minp, maxp, seed)
+	local blockpos = cs_map.origin_pos
+	if(minp.x <= blockpos.x and maxp.x >= blockpos.x and minp.y <= blockpos.y and maxp.y >= blockpos.y and minp.z <= blockpos.z and maxp.z >= blockpos.z) then
+		minetest.set_node(blockpos, {name = "cs_map:origin"})
+		minetest.log("action", "[cs_map] origin has been set at "..minetest.pos_to_string(blockpos)..".")
+	end
+end)
