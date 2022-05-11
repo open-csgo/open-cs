3c2579f6349f2590313c823ff5e3d64203030bc0
AFCMS <afcm.contact@gmail.com>
2022-05-11 10:48:26 +0200

    fix

--- mods/CS/cs_debug/init.lua
+++ mods/CS/cs_debug/init.lua
@@ -1,10 +1,19 @@
 local dump = dump
+local C = minetest.colorize
 
 minetest.register_chatcommand("ldebug", {
 	description = "List things registered in the engine",
 	func = function(name, param)
 		if param == "abm" then
-			return true, string.format("There are %s abms\n", #minetest.registered_abms)..dump(minetest.registered_abms)
+			return true,
+				C(string.format("There are %s abms\n=============\n", #minetest.registered_abms), "green")..
+				dump(minetest.registered_abms)..
+				C("=============", "green")
+		elseif param == "nodes" then
+			return true,
+				C("green", string.format("There are %s nodes\n=============\n", #minetest.registered_nodes))..
+				dump(minetest.registered_nodes)..
+				C("green", "=============")
 		else
 			return false, "No such param"
 		end
