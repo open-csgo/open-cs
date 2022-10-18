c16745c0ae8a277da555b5c792b62f374e6ee0a9
AFCMS <afcm.contact@gmail.com>
2022-10-19 00:40:37 +0200

    Stuff

--- mods/CS/cs_data/init.lua
+++ mods/CS/cs_data/init.lua
@@ -1,13 +1,28 @@
 local ipairs = ipairs
 
-local data = {t=255}
-
+---@type table<string,ModChannel>
 local players_channels = {}
 
 local queue = {}
 
+cs_data = {}
+
+local has_started = false
+
+---@type table<string,any>
+cs_data.registered_initial_data = {}
+
+---@param name string
+---@param data any
+function cs_data.register_initial_data(name, data)
+	assert(has_started == false, "Cannot register initial data at runtime!")
+	cs_data.registered_initial_data[name] = data
+end
+
+-- JOIN MOD CHANNELS
+
 minetest.register_on_joinplayer(function(player)
-	players_channels[player] = minetest.mod_channel_join("cs_client:join_data:"..player:get_player_name())
+	players_channels[player] = minetest.mod_channel_join("cs_client:join_data:" .. player:get_player_name())
 	table.insert(queue, player)
 end)
 
@@ -16,11 +31,12 @@ minetest.register_on_leaveplayer(function(player)
 end)
 
 minetest.register_globalstep(function(dtime)
+	has_started = true
 	local r = {}
-	for _,o in ipairs(queue) do
+	for _, o in ipairs(queue) do
 		if players_channels[o]:is_writeable() then
-			minetest.after(5, function ()
-				players_channels[o]:send_all(minetest.serialize(data))
+			minetest.after(5, function()
+				players_channels[o]:send_all(minetest.serialize(cs_data.registered_initial_data))
 			end)
 		else
 			table.insert(r, o)
@@ -29,3 +45,4 @@ minetest.register_globalstep(function(dtime)
 	queue = r
 end)
 
+cs_data.register_initial_data("cs_data:test", 5)
